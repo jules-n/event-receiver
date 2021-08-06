bd109435f1d8f579a20c16cb95d84196f2003e43
ynero <ynero@softserveinc.com>
2021-08-06 14:35:55 +0300

    Added service for adding tenant-id

--- src/main/java/com/example/tenantrecognizer/services/TenantParameterService.java
+++ src/main/java/com/example/tenantrecognizer/services/TenantParameterService.java
@@ -1,28 +1,29 @@
-package com.example.tenantrecognizer.config;
+package com.example.tenantrecognizer.services;
 
+import com.example.tenantrecognizer.config.TenantIdConfig;
 import com.example.tenantrecognizer.recognizer.TenantRecognizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
 
-@Configuration
-public class TenantParameterConfig {
+@Service
+public class TenantParameterService {
 
     @Autowired
     private TenantIdConfig tenantIdConfig;
 
+    @Autowired
     private TenantRecognizer tenantRecognizer;
 
-    public TenantParameterConfig(){
-        tenantRecognizer = new TenantRecognizer();
-    }
+    @Value("${device-service-url}")
+    private String url;
 
-    @Bean
-    public WebClient addTenant(String url){
+
+
+    public WebClient addTenant(){
         var webClient =  WebClient.builder().baseUrl(url);
         if(tenantIdConfig.getBy().equals(TenantIdConfig.BY.REQUEST_PARAM))
             webClient.defaultUriVariables(new HashMap<>(){{put(tenantIdConfig.getRequestParamName(),tenantRecognizer.getTenant());}});
