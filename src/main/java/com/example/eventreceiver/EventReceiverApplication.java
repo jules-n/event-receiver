4aebaa82a34402c549be8ecacd77b413b47bd378
ynero <ynero@softserveinc.com>
2021-08-09 16:27:44 +0300

    SS-48: Removed old implementation of initial tenant recognizer

--- src/main/java/com/example/eventreceiver/EventReceiverApplication.java
+++ src/main/java/com/example/eventreceiver/EventReceiverApplication.java
@@ -1,12 +1,9 @@
 package com.example.eventreceiver;
 
-import com.example.eventreceiver.config.TenantIdConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(TenantIdConfig.class)
 public class EventReceiverApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/eventreceiver/services/TenantParameterService.java
+++ src/main/java/com/example/eventreceiver/services/TenantParameterService.java
@@ -1,13 +1,8 @@
 package com.example.eventreceiver.services;
 
-import com.example.eventreceiver.config.TenantIdConfig;
 import com.example.eventreceiver.recognizer.TenantRecognizer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.HashMap;
 
 @Service
 public class TenantParameterService {
@@ -17,20 +12,5 @@ public class TenantParameterService {
 
     @Autowired
     private TenantRecognizer tenantRecognizer;
-
-   /* @Value("${device-service-url}")
-    private String url;
-
-
-    public WebClient addTenant() {
-        var webClient = WebClient.builder().baseUrl(url);
-        var isMustBeAttachedHeader = tenantIdConfig.getBy().equals(TenantIdConfig.BY.HEADER);
-        if (isMustBeAttachedHeader)
-            webClient.defaultUriVariables(new HashMap<>() {{
-                put(tenantIdConfig.getRequestParamName(), tenantRecognizer.getTenant());
-            }});
-        else webClient.defaultHeader(tenantIdConfig.getHeaderName(), tenantRecognizer.getTenant());
-        return webClient.build();
-    }*/
 
 }
