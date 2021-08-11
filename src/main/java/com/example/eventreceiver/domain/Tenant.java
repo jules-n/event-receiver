7b60361ae2b6527c8bd867f45599f5720a757d2d
ynero <ynero@softserveinc.com>
2021-08-11 03:24:35 +0300

    SS-1:Domain model of tenant

--- src/main/java/com/example/eventreceiver/domain/Tenant.java
+++ src/main/java/com/example/eventreceiver/domain/Tenant.java
@@ -1,2 +1,17 @@
-package com.example.eventreceiver.domain;public class Tenant {
+package com.example.eventreceiver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "tenants")
+public class Tenant {
+    private String tenantId;
+    private String[] topics;
+    private String[] urls;
 }
