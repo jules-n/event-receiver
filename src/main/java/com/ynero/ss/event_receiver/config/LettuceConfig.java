453235965c98ed93389a5c8c46da37ee3b6f201d
ynero <ynero@softserveinc.com>
2021-09-14 17:49:10 +0300

    feature/SS-80: Added bean of redis template

--- src/main/java/com/ynero/ss/event_receiver/config/LettuceConfig.java
+++ src/main/java/com/ynero/ss/event_receiver/config/LettuceConfig.java
@@ -4,8 +4,10 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class LettuceConfig {
@@ -19,5 +21,12 @@ public class LettuceConfig {
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(hostName, port));
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
