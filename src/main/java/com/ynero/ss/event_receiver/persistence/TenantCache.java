16f977f82930f7f615509267c5d1566030ebe0cf
ynero <ynero@softserveinc.com>
2021-09-16 11:23:29 +0300

    feature/SS-80: Changed hash to value ops

--- src/main/java/com/ynero/ss/event_receiver/persistence/TenantCache.java
+++ src/main/java/com/ynero/ss/event_receiver/persistence/TenantCache.java
@@ -3,8 +3,11 @@ package com.ynero.ss.event_receiver.persistence;
 import com.ynero.ss.event_receiver.domain.Tenant;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class TenantCache {
@@ -12,12 +15,26 @@ public class TenantCache {
     @Setter(onMethod_ = {@Autowired})
     private RedisTemplate<String, Object> redisTemplate;
 
-    private final String tenantsHashKey = "TENANTS";
-    private final String urlsHashKey = "URLS";
-    private final String topicsHashKey = "TOPICS";
+    @Setter(onMethod_ = {@Value("${spring.data.redis.expiration}")})
+    private int expirationTime;
+
+    private String tenantIdWithPrefix(String tenantId) {
+        String tenantPrefix = "tenant: ";
+        return tenantPrefix + tenantId;
+    }
+
+    private String topicWithPrefix(String topic) {
+        String topicPrefix = "tenant:by-topic: ";
+        return topicPrefix + topic;
+    }
+
+    private String urlWithPrefix(String url) {
+        String urlPrefix = "tenant:by-url: ";
+        return urlPrefix + url;
+    }
 
     public void save(Tenant tenant) {
-        redisTemplate.opsForHash().put(tenantsHashKey, tenant.getTenantId(), tenant);
+        redisTemplate.opsForValue().set(tenantIdWithPrefix(tenant.getTenantId()), tenant, expirationTime, TimeUnit.SECONDS);
         tenant.getTopics().forEach(
                 topic -> addTopic(topic, tenant.getTenantId())
         );
@@ -27,32 +44,32 @@ public class TenantCache {
     }
 
     public void delete(Tenant tenant) {
-        redisTemplate.opsForHash().delete(tenantsHashKey, tenant.getTenantId());
+        redisTemplate.delete(tenantIdWithPrefix(tenant.getTenantId()));
         tenant.getTopics().forEach(
-                topic -> redisTemplate.opsForHash().delete(topicsHashKey, topic)
+                topic -> redisTemplate.delete(topicWithPrefix(topic))
         );
         tenant.getUrls().forEach(
-                url -> redisTemplate.opsForHash().delete(urlsHashKey, url)
+                url -> redisTemplate.delete(urlWithPrefix(url))
         );
     }
 
     public String findByTopic(String topic) {
-        return (String) redisTemplate.opsForHash().get(topicsHashKey, topic);
+        return (String) redisTemplate.opsForValue().get(topicWithPrefix(topic));
     }
 
     public void addTopic(String topic, String tenantId) {
-        redisTemplate.opsForHash().put(topicsHashKey, topic, tenantId);
+        redisTemplate.opsForValue().set(topicWithPrefix(topic), tenantIdWithPrefix(tenantId), expirationTime, TimeUnit.SECONDS);
     }
 
     public void addURL(String url, String tenantId) {
-        redisTemplate.opsForHash().put(urlsHashKey, url, tenantId);
+        redisTemplate.opsForValue().set(urlWithPrefix(url), tenantIdWithPrefix(tenantId), expirationTime, TimeUnit.SECONDS);
     }
 
     public String findByURL(String url) {
-        return (String) redisTemplate.opsForHash().get(urlsHashKey, url);
+        return (String) redisTemplate.opsForValue().get(urlWithPrefix(url));
     }
 
     public Tenant findByTenantId(String tenantId) {
-        return (Tenant) redisTemplate.opsForHash().get(tenantsHashKey, tenantId);
+        return (Tenant) redisTemplate.opsForValue().get(tenantIdWithPrefix(tenantId));
     }
 }
