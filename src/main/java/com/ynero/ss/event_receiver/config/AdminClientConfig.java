a1d1db52b55869d84428a51888f2489d3928a9b8
ynero <ynero@softserveinc.com>
2021-10-07 13:45:39 +0300

    feature/SS-33: Fixed Failed to load ApplicationContext

--- src/main/java/com/ynero/ss/event_receiver/config/AdminClientConfig.java
+++ src/main/java/com/ynero/ss/event_receiver/config/AdminClientConfig.java
@@ -4,8 +4,6 @@ import lombok.Setter;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
